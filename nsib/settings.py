c584eb411ad02ac7453dcc70c3582165a7a3026c
Olumide <o3cloudng@gmail.com>
2023-08-16 13:27:55 +0100

    App dockerization

--- nsib/settings.py
+++ nsib/settings.py
@@ -7,9 +7,10 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = os.environ.get("DEBUG", default=True)
 PRODUCTION = int(os.environ.get("PRODUCTION", default=1))
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
