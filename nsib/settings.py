3d46fe160b10d2be1c9a40764488351cbf357c67
Olumide <o3cloudng@gmail.com>
2023-08-23 14:41:16 +0100

    Modal dev

--- nsib/settings.py
+++ nsib/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", default=True)
 PRODUCTION = int(os.environ.get("PRODUCTION", default=1))
 # ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
