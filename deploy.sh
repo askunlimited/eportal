a4b287803b09e58dbb2ce4b43b8819b27e0b9931
Olumide <o3cloudng@gmail.com>
2023-08-29 20:13:58 +0100

    Updated Document pdf view

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
+cd /home/django/
 docker stop eportal
 docker rm eportal
 docker rmi askunlimited/eportal:latest
 docker pull askunlimited/eportal:latest
-docker run -t -d -p 80:8000 --name=eportal --env-file ./.env askunlimited/eportal:latest
+docker run --network nginxproxymanager_default --env-file ./.env --name=eportal -d askunlimited/eportal:latest
+
+
+# docker run -t -d -p 80:8000 --name=eportal --env-file ./.env askunlimited/eportal:latest
 # docker run --network nginxproxymanager_default --env-file ./.env --name=eportal -p 80:8000 -d askunlimited/eportal
 #  chmod a+x deply.sh
 
